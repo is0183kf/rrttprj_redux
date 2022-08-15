71f4b901f3e0bc514d5726d7f4cd575e0bb657c0
Yudai Higuchi <is0183kf@gmail.com>
2022-08-15 18:00:20 +0900

    Delete counter feature, fix coding style

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { store } from './app/store'
-import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
